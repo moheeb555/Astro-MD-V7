0b0b33100822a5fa1411b92f6878f693ad147865
moheeb555 <124988811+moheeb555@users.noreply.github.com>
2023-02-17 15:57:36 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀ
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=JmZRwSwB#Sx7vpBW5ozo0SFopSkSWehNEyx5hn-EiT0i21PFR0kE' : process.env.SESSION_ID
 };
